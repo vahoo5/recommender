a5cd64082938cb4a08a2f66c73e6b94a089758f0
Your Name <you@example.com>
2021-03-11 16:32:20 +0100

    added shit

--- myserver/recommender_main/management/commands/recommend.py
+++ myserver/recommender_main/management/commands/recommend.py
@@ -36,9 +36,34 @@ def get_time_in_min(s):
                 time_in_min += time
     return time_in_min
     
+def clean(animes):
+    animes['duration'] = animes['duration'].apply(lambda x: get_time_in_min(x))
+    animes['description'] = animes['description'].apply(lambda x: re.sub('\[Source.*\]|\(Source.*\)|Source.*|\[.*(by|By).*\]','',x).strip())
+    animes['genres'] = animes['genres'].apply(lambda x: [y.replace(' ','') for y in x.split(',')])
+    animes['metadata'] = animes.apply(lambda x : ' '.join(x['genres']), axis = 1)
+    animes = animes[(lambda x: [True if y in ['TV', 'Movie'] else False for y in x])(animes['kind'])]
+    animes = animes[(lambda x: [True if y>15 else False for y in x])(animes['duration'])]
+    animes.id = [x for x in range(animes.shape[0])]
+
+
 class Command(BaseCommand):
     help = 'calculate recommendations'
 
     def handle(self, *args, **kwargs):
-        df = pd.DataFrame(list(Anime.objects.filter(id__lte=100).values()))
-        print(df.head())
+        animes = pd.DataFrame(list(Anime.objects.filter(id__lte=150).values()))
+        animes = clean(animes)
+
+        tf = TfidfVectorizer(stop_words='english')
+        tfidf_matrix = tf.fit_transform(animes['description'])
+        cosine_similarities_description = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+        count_vec = CountVectorizer(stop_words='english')
+        count_vec_matrix = count_vec.fit_transform(animes['metadata'])
+
+        cosine_similarities_metadata = cosine_similarity(count_vec_matrix, count_vec_matrix)
+        cosine_similarities_07 = (cosine_similarities_description * 0.3 + cosine_similarities_metadata * 0.7) / 2
+
+        results_1 = getSimilar(cosine_similarities_metadata)
+        results_07 = getSimilar(cosine_similarities_07)
+        toJson(results_1, '1')
+        toJson(results_07, '07')
